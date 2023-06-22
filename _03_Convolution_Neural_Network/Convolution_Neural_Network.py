4a03d17fa2470633f59ba1558f74c5c95302e3cd
= <=>
2023-06-22 22:01:32 +0800

    !

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,10 +1,7 @@
-# 在该文件NeuralNetwork类中定义你的模型 
-# 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
-
 import os
 
-os.system("sudo pip3 install torch")
-os.system("sudo pip3 install torchvision")
+#os.system("sudo pip3 install torch")
+#os.system("sudo pip3 install torchvision")
 
 import torch
 import torch.nn as nn
@@ -12,24 +9,72 @@ import torch.optim as optim
 import torchvision
 
 from torch.utils.data import DataLoader
-    
+
 
 class NeuralNetwork(nn.Module):
-    pass
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        # 五个卷积层
+        self.conv1 = nn.Sequential(  # 输入 32 * 32 * 3
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=3, stride=1, padding=1),  # (32-3+2)/1+1 = 32
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # (32-2)/2+1 = 16
+        )
+        self.conv2 = nn.Sequential(  # 输入 16 * 16 * 6
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3, stride=1, padding=1),  # (16-3+2)/1+1 = 16
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # (16-2)/2+1 = 8
+        )
+        self.conv3 = nn.Sequential(  # 输入 8 * 8 * 16
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),  # (8-3+2)/1+1 = 8
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # (8-2)/2+1 = 4
+        )
+        self.conv4 = nn.Sequential(  # 输入 4 * 4 * 64
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),  # (4-3+2)/1+1 = 4
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # (4-2)/2+1 = 2
+        )
+        self.conv5 = nn.Sequential(  # 输入 2 * 2 * 128
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),  # (2-3+2)/1+1 = 2
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # (2-2)/2+1 = 1
+        )  # 最后一层卷积层，输出 1 * 1 * 128
+        # 全连接层
+        self.dense = nn.Sequential(
+            nn.Linear(128, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = x.view(-1, 128)
+        x = self.dense(x)
+        return x
+
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
     # 保持本地训练的数据读取和这里一致
-    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
+    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
+                                                 transform=torchvision.transforms.ToTensor())
+    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False,
+                                               transform=torchvision.transforms.ToTensor())
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
     data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
+
 def main():
-    model = NeuralNetwork() # 若有参数则传入参数
+    model = NeuralNetwork()  # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
     return model
-    
